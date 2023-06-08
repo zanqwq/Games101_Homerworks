1dff2c55e03931260fd25da01e68c37a08982748
root <root@LAPTOP-8412K6DC.localdomain>
2023-06-09 03:25:38 +0800

    fix: divide bounds area

--- Assignment6/PA6/Assignment6/BVH.cpp
+++ Assignment6/PA6/Assignment6/BVH.cpp
@@ -114,8 +114,8 @@ BVHBuildNode* BVHAccel::recursiveBuildWithSAH(std::vector<Object*> objects)
         return node;
     }
     else if (objects.size() == 2) {
-        node->left = recursiveBuild(std::vector{objects[0]});
-        node->right = recursiveBuild(std::vector{objects[1]});
+        node->left = recursiveBuildWithSAH(std::vector{objects[0]});
+        node->right = recursiveBuildWithSAH(std::vector{objects[1]});
 
         node->bounds = Union(node->left->bounds, node->right->bounds);
         return node;
@@ -171,8 +171,8 @@ BVHBuildNode* BVHAccel::recursiveBuildWithSAH(std::vector<Object*> objects)
             }
             cost[i] =
                 costRayBoxIntersection
-                + (cnt0 * costRayPrimitiveIntersection * b0.SurfaceArea())
-                + (cnt1 * costRayPrimitiveIntersection * b1.SurfaceArea());
+                + (cnt0 * costRayPrimitiveIntersection * b0.SurfaceArea() / bounds.SurfaceArea())
+                + (cnt1 * costRayPrimitiveIntersection * b1.SurfaceArea() / bounds.SurfaceArea());
             if (cost[i] < minCost) {
                 minCost = cost[i];
                 minCostSplitBucketIdx = i;
