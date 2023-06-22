9e12eda1d12417fd5b9efcb211c821c05f99552e
root <root@LAPTOP-8412K6DC.localdomain>
2023-06-22 12:03:03 +0800

    feat: euler

--- Assignment8/assignment8/src/rope.cpp
+++ Assignment8/assignment8/src/rope.cpp
@@ -12,11 +12,19 @@ namespace CGL {
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
         // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+        for (int i = 0; i < num_nodes; i++) {
+            auto node_pos = start + (end - start) * ((double)i / (num_nodes - 1));
+            masses.push_back(new Mass(node_pos, node_mass, false));
+
+            if (i != 0) {
+                springs.push_back(new Spring(masses[i - 1], masses[i], k));
+            }
+        }
 
 //        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+        for (auto &i : pinned_nodes) {
+            masses[i]->pinned = true;
+        }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
@@ -24,6 +32,21 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 2): Use Hooke's law to calculate the force on a node
+            // 胡克定律: F_a2b = k * DirA2B * (||b - a|| - l)
+            auto k = s->k;
+            auto a = s->m1, b = s->m2;
+            auto posA = a->position, posB = b->position;
+            auto vecA2B = (posB - posA).unit();
+            auto lenDelta = (posB - posA).norm() - s->rest_length;
+            auto fA2B = k * vecA2B * lenDelta;
+            a->forces = fA2B;
+            b->forces = -fA2B;
+
+            // internal spring damping
+            auto k_d = 0.5;
+            auto f_d = -k_d * dot(b->velocity - a->velocity, vecA2B) * vecA2B;
+            a->forces += f_d;
+            b->forces += -f_d;
         }
 
         for (auto &m : masses)
@@ -31,8 +54,16 @@ namespace CGL {
             if (!m->pinned)
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+                m->forces += gravity * m->mass;
 
                 // TODO (Part 2): Add global damping
+                auto k_d = 0.5;
+                m->forces += -k_d * m->velocity;
+
+                auto acceleration = m->forces / m->mass;
+
+                m->position += m->velocity * delta_t;
+                m->velocity += acceleration * delta_t;
             }
 
             // Reset all forces on each mass
